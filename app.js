23ad93c72dfd6672d9ab84043d8042a2c3b70860
123 <67495566+sdh010@users.noreply.github.com>
2025-12-03 11:22:19 +0900

    Update footer text in app.js

--- app.js
+++ app.js
@@ -264,7 +264,7 @@ app.get('/', (req, res) => {
       <div class="container">
         <div class="header">
           <h1>🚀 ${APP_NAME}</h1>
-          <p>웹 애플리케이션 - TodoList, 연락처 폼</p>
+          <p>20232485 서동현</p>
         </div>
 
         <div class="tabs">
